b80cd1d94415f885222293edeef7f1f555c821f6
ssjh96 <ssjh96@outlook.com>
2025-01-25 22:46:25 +0800

    base added

---
+++ src/main/java/vttp5/paf/day27ws2/Day27ws2Application.java
@@ -0,0 +1,13 @@
+package vttp5.paf.day27ws2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day27ws2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day27ws2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp5/paf/day27ws2/Day27ws2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp5.paf.day27ws2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day27ws2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
