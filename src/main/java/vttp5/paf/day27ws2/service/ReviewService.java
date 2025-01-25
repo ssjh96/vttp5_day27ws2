a0a463c20d7ac1fe48098eee8c18897f5a04144c
ssjh96 <ssjh96@outlook.com>
2025-01-26 00:32:01 +0800

    completed part a

--- src/main/java/vttp5/paf/day27ws2/service/ReviewService.java
+++ src/main/java/vttp5/paf/day27ws2/service/ReviewService.java
@@ -36,10 +36,13 @@ public class ReviewService {
 
         Document gameDoc = optGameDoc.get();
 
-        // Create review JSON
+        // Create review JSON, (2 options)
+        // 1. Document docReviewToInsert = new Document().append("_id", new ObjectId()).append("user", user)
+        // 2. Use JsonObjectBuilder and append as required after converting to Document
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         JsonObject jReview = Json.createObjectBuilder()
-                                .add("_id", gameDoc.getObjectId("_id").toString())
+                                .add("_id", gameDoc.getObjectId("_id").toString()) 
+                                // This would not give ObjectId("") because it is save as a string and not a ObjectId object, but just the ""
                                 .add("user", user)
                                 .add("rating", rating)
                                 .add("comment", comment != null ? comment : "")
@@ -50,6 +53,8 @@ public class ReviewService {
                              
         // Insert review into MongoDB                       
         Document docReviewToInsert = Document.parse(jReview.toString());
+        docReviewToInsert.append("_id", gameDoc.getObjectId("_id")); // This will appear on top as it is unique primary
+
         reviewRepo.insertReview(docReviewToInsert);
        
 
